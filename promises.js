48651e839dfc6a9c09d496a0afc3c224187a8294
Mateusz Swiatkowski <6883643+matvs@users.noreply.github.com>
2019-08-13 18:31:00 +0200

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,1 +1,44 @@
+let createPromise = ms => new Promise((resolve, reject) => {
+  setTimeout(() => resolve(ms), ms) 
+});
 
+let log = info => console.log(info);
+
+let promise1 = createPromise(1);
+let promise2 = createPromise(2);
+let promise3 = createPromise(3);
+let promise4 = createPromise(4);
+
+let allThen = () => Promise.all([promise3, promise4]).then(() => {})
+let all = () => Promise.all([promise3, promise4])
+
+promise1.then(data => {
+  log(data);
+  return promise2;
+}).then(log)
+
+promise1.then(data => {
+  log(data);
+  return promise2.then(data => {
+    log(data);
+    return promise3;
+  });
+}).then(log)
+
+
+promise1.then(data => {
+  log(data);
+  return promise2.then(data => {
+    log(data);
+    return allThen();
+  });
+}).then(log)
+
+
+promise1.then(data => {
+  log(data);
+  return promise2.then(data => {
+    log(data);
+    return all();
+  });
+}).then(log)
