92ab62f2ef63e45a421471a7f57c4df0ee515d32
Mateusz Swiatkowski <6883643+matvs@users.noreply.github.com>
2019-12-05 14:50:18 +0100

    Update bytesToHumanReadable.js
    Formatting, keywords

--- bytesToHumanReadable.js
+++ bytesToHumanReadable.js
@@ -1,9 +1,8 @@
-units = ['bytes', 'kB', 'MB', 'GB', 'TB']
+const units = ['bytes', 'kB', 'MB', 'GB', 'TB']
 function bytesToHumanReadable(bytes) {
-	level = Math.floor(Math.log(bytes)/Math.log(1024));
-	if (level >= units.length) {
-		level = units.length - 1;
+    let level = Math.floor(Math.log(bytes)/Math.log(1024));
+    if (level >= units.length) {
+        level = units.length - 1;
     }
-	return (bytes/Math.pow(1024, level)).toFixed(2) + ' ' + units[level];
-
+    return (bytes/Math.pow(1024, level)).toFixed(2) + ' ' + units[level];
 }
