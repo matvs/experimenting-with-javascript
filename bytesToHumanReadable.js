2549c7e06ac75681188570433d3c062edc491d3b
Mateusz Swiatkowski <6883643+matvs@users.noreply.github.com>
2019-12-05 14:48:19 +0100

    Create bytesToHumanReadable.js

---
+++ bytesToHumanReadable.js
@@ -0,0 +1,9 @@
+units = ['bytes', 'kB', 'MB', 'GB', 'TB']
+function bytesToHumanReadable(bytes) {
+	level = Math.floor(Math.log(bytes)/Math.log(1024));
+	if (level >= units.length) {
+		level = units.length - 1;
+    }
+	return (bytes/Math.pow(1024, level)).toFixed(2) + ' ' + units[level];
+
+}
