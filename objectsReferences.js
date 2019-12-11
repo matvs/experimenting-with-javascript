efcde3a9d88c2b5c4a256233a77b07fdf1adf7ea
Mateusz Swiatkowski <6883643+matvs@users.noreply.github.com>
2019-12-11 15:09:04 +0100

    Update objectsReferences.js

--- objectsReferences.js
+++ objectsReferences.js
@@ -1,4 +1,18 @@
-let arrayOfObjects = [ { prop: 1 }, { prop: 2 }, { prop: 3 } ]
-let copyOfArrayOfObjects = arrayOfObjects.slice()
+var arrayOfObjects = [ { prop: 1 }, { prop: 2 }, { prop: 3 } ]
+var copyOfArrayOfObjects = arrayOfObjects.slice()
+
 arrayOfObjects.push(3)
 arrayOfObjects[0].prop = "NEW VALUE"
+print();
+
+arrayOfObjects[0] = null;
+var myObject = arrayOfObjects[1];
+myObject.prop = "Yet another new value";
+print();
+
+Object.assign(arrayOfObjects[2], {newProp: 'Elo'});
+
+function print(){
+  console.log(arrayOfObjects);
+  console.log(copyOfArrayOfObjects);
+}
